e422b98109f290803da5bf0ca6e7f83e9f4b1e01
Joel-java-jpg <Joel.schou@iths.se>
2026-05-23 20:12:41 +0200

    Add email sending with JavaMailSender

--- src/main/java/se/iths/joel/emailservice/service/EmailService.java
+++ src/main/java/se/iths/joel/emailservice/service/EmailService.java
@@ -1,30 +1,35 @@
 package se.iths.joel.emailservice.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    private static final Logger log =
-            LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
 
-    public void sendEmail(
-            String email,
-            String customerName,
-            String productName,
-            Integer quantity) {
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
-        log.info("""
-                        Sending email to: {}
-                        Customer: {}
-                        Product: {}
-                        Quantity: {}
-                        """,
-                email,
-                customerName,
-                productName,
-                quantity);
+    public void sendEmail(String email,
+                          String customerName,
+                          String productName,
+                          Integer quantity) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(email);
+        message.setSubject("Order Confirmation");
+
+        message.setText(
+                "Hej " + customerName + "\n\n" +
+                        "Din beställning har registrerats.\n" +
+                        "Produkt: " + productName + "\n" +
+                        "Antal: " + quantity
+        );
+
+        mailSender.send(message);
     }
 }
