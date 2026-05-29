f00acff0c41bbdff91ca75c177fa8c99a1e5a643
klasen98 <klasen98@live.se>
2026-05-29 09:32:17 +0200

    fixa så vi synkar våra klasser

--- src/main/java/se/iths/joel/emailservice/dto/OrderItemMessage.java
+++ src/main/java/se/iths/joel/emailservice/dto/OrderItemMessage.java
@@ -2,11 +2,9 @@ package se.iths.joel.emailservice.dto;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class OrderItemMessage {
     private String name;
-    private BigDecimal price;
+    private double price;
     private int quantity;
 }
